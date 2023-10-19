a8ca988333d467f463f2b5b1d53bc568919237f0
jruocco <jruocco98@gmail.com>
2023-10-19 15:32:00 -0500

    test

---
+++ src/Pages/Login.js
@@ -0,0 +1,15 @@
+
+import './Login.css';
+
+function Login() {
+  return (
+    <div className='Login'>
+      <header>
+        Login Page
+      </header>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
